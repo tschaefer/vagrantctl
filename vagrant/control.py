1be5be69b53d82711f10e100c7566719e885e854
tschaefer <github@blackox.org>
2016-01-20 19:50:54 +0100

    use package vagrant

--- vagrant/control.py
+++ vagrant/control.py
@@ -1,32 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import os
-import subprocess
+from vagrant import Vagrant
 
 
-class VagrantControl(object):
-
-    def __init__(self, base_directory=None):
-        self.base_directory = os.path.abspath(base_directory)
+class VagrantControl(Vagrant):
 
     def list(self):
         containers = list()
-        for root, dirs, files in os.walk(self.base_directory):
+        for root, dirs, files in os.walk(self.root):
             for file in files:
                 if file == 'Vagrantfile':
                     containers.append(os.path.basename(root))
         return containers
-
-    def start(self):
-        return
-
-    def stop(self):
-        return
-
-    def status(self, container):
-        cwd = os.path.join(self.base_directory, container)
-        process = subprocess.Popen(['vagrant', 'status', '--machine-readable'],
-                                   cwd=cwd,
-                                   stdout=subprocess.PIPE)
-        output = process.communicate()
-        return output[0].split('\n')[2].split(',')
