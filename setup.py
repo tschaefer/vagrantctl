a3f3cd475df92385e711cb6c7477a4d6c10bcb12
tschaefer <github@blackox.org>
2016-01-28 09:24:56 +0100

    added install requirements

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='vagrantctl',
     version='0.0.1',
     packages=['vagrantctl'],
-    install_requires=[],
+    install_requires=['python-vagrant >= 0.5.10'],
     entry_points={'console_scripts': ['vagrantctl=vagrantctl.__main__:main']},
     author='Tobias Schäfer',
     author_email='vagrantctl@blackoxorg',
